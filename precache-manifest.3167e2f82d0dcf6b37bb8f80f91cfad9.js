a57cb618ab6cbc5ee9b877a2411abbd329aa4446
dilarabikmaz <handandilara.bikmaz@yale.edu>
2024-07-31 13:50:48 +0300

    Updates

---
+++ precache-manifest.3167e2f82d0dcf6b37bb8f80f91cfad9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ebf07762b015af20541cc9be8a9fc7e",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "2ee8ba33bbc45da89402",
+    "url": "/personal-website/static/css/main.a590e56f.chunk.css"
+  },
+  {
+    "revision": "d056a3f4b4689fffe3c0",
+    "url": "/personal-website/static/js/2.5d58a67a.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/personal-website/static/js/2.5d58a67a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b853d2541dec90ed5ec1",
+    "url": "/personal-website/static/js/3.bd55f824.chunk.js"
+  },
+  {
+    "revision": "2ee8ba33bbc45da89402",
+    "url": "/personal-website/static/js/main.53dd3fd6.chunk.js"
+  },
+  {
+    "revision": "424f65ec49ba4823a8c4",
+    "url": "/personal-website/static/js/runtime-main.dd5454b7.js"
+  },
+  {
+    "revision": "ce4a3cbcfddb04b37fe63e868f1ba8e3",
+    "url": "/personal-website/static/media/profile.ce4a3cbc.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0bf4248f43db17d03ac6fdb906779945.js"
+  "/personal-website/precache-manifest.3167e2f82d0dcf6b37bb8f80f91cfad9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
